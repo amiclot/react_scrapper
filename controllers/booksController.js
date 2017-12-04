9943a76416d0ee474d7b7975355f3bd891bbd6bf
Adam Miclot <amiclot09@gmail.com>
2017-12-04 11:27:24 -0500

    Added scraper, feeds to html instead of book list

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,34 +1,60 @@
 const db = require("../models");
+var cheerio = require("cheerio");
+var request = require("request");
 
-// Defining methods for the booksController
+// Defining methods for the ArticlesController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Article
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Article
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+    var article = {};
+    request("http://www.graciemag.com/en/", function(err, request, html)
+    {
+        if(err){throw err}
+        var $ = cheerio.load(html)
+        $("div.post-two").each(function(i, element)
+        {
+            var title = $(element).find("div.iten-right").find("h3").find("a").text();
+            var info = $(element).find("div.iten-right").find("p").text();
+            var img = $(element).find("img").attr("src");
+            var link = $(element).find("div.iten-right").find("h3").find("a").attr("href");
+            
+
+            var article = 
+            {
+                title: title,
+                info: info,
+                img: img,
+                link: link
+            }
+
+            
+        db.Article.create(article)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+        });
+            
+    });
   },
   update: function(req, res) {
-    db.Book
+    db.Article
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Article
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
--- models/book.js
+++ models/book.js
@@ -1,13 +1,44 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+var mongoose = require("mongoose");
 
-const bookSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: false
+  },
+  img: {
+    type: String,
+    required: false
+  },
+  info: {
+    type: String,
+    required: false
+  },
+  // `link` is required and of type String
+  link: {
+    type: String,
+    required: false
+  },
+
+
+
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
-const Book = mongoose.model("Book", bookSchema);
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
 
-module.exports = Book;
+
+// Export the Article model
+module.exports = Article;
